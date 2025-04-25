7784d201b571929cf9b93b0ad7a912c0138ded48
nunes <pehennu@gmail.com>
2025-04-25 19:02:46 -0300

    feat: commit inicial do projeto

---
+++ src/test/java/com/br/luizalab/comunicacao/ComunicacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.luizalab.comunicacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComunicacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
